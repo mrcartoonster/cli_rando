88ae882e37f1062f99181ef282513a2bf9efe8b9
evan <evan@pop-os.localdomain>
2022-12-02 21:37:24 -0700

    RAN-1 #close #comment passing typer tests #time 30m

---
+++ tests/test_typer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Typer testing.
+"""
+from typer.testing import CliRunner
+
+from src.tempo_random.main import app
+
+runner = CliRunner()
+
+
+def test_tempo_defualts():
+    """
+    Testing defaults.
+    """
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert "Set tempo" in result.stdout
