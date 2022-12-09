0cdb7ec0beb29bdb8ba9b3db9518889fbbef629b
evan <evan@pop-os.localdomain>
2022-12-08 19:03:24 -0700

    RAN-3 #comment Added paramtrized tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,7 +2,12 @@
 """
 Will have the tests for functions here.
 """
+import pytest
+
 from src.tempo_random.functions import tempo_generate
+
+set_tempos = [(50, 60), (60, 70), (70, 80)]
+tempo_ids = [str(_) for _ in set_tempos]
 
 
 def test_generate():
@@ -20,4 +25,14 @@ def test_default():
     Test the defualt which is 50 and 100.
     """
     sample = tempo_generate(50, 100)
-    assert sample > 50 and sample < 100
+    assert sample >= 50 and sample <= 100
+
+
+@pytest.mark.smoke
+@pytest.mark.parametrize("numbers", set_tempos, ids=tempo_ids)
+def test_multiples(numbers):
+    """
+    Testing multiples.
+    """
+    sample = tempo_generate(*numbers)
+    assert sample >= numbers[0] and sample <= numbers[1]
